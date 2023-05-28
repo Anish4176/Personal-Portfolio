11bfbeac90c9d2fc9bc87eaf1cabee136fd6f50a
Anish <anishsingh41764176@gmail.com>
2023-05-29 00:55:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <BrowserRouter>
 
       <div>
-        <div className="h-[100vh]">
+        <div className="h-[100vh] ">
           <Navbar />
           <hr />
           <Mainsection />
